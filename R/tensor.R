c1f265ce6a66c7e9d870027ffc7479197edebac7
Daniel Falbel <dfalbel@gmail.com>
2018-11-27 18:32:56 -0200

    make website cool

--- R/tensor.R
+++ R/tensor.R
@@ -21,33 +21,33 @@ tensor <- function(x) {
   `torch::Tensor`$dispatch(tensor_(x, dimension))
 }
 
-#' as.array
+#' Tensor casting
 #'
-#' Casts a `torch::Tensor` to an R array
+#' Casts an object with class [tensor] to an R atomic vector, matrix or array.
 #'
-#' @param x torch::Tensor object to be casted to an R array.
+#' @param x tensor object to be casted to an R array.
 #'
 #' @examples
 #' x <- tensor(array(1:8, dim = c(2, 2, 2)))
 #' as.array(x)
 #'
 #' @export
-`as.array.tensor` <- function(x) {
+as.array.tensor <- function(x) {
   x$as_vector()
 }
 
 #' @export
-`as.matrix.tensor` <- function(x) {
+as.matrix.tensor <- function(x) {
   as.matrix(x$as_vector())
 }
 
 #' @export
-`abs.tensor` <- function(x) {
+abs.tensor <- function(x) {
   x$abs()
 }
 
 #' @export
-`acos.tensor` <- function(x) {
+acos.tensor <- function(x) {
   x$acos()
 }
 
@@ -57,21 +57,21 @@ tensor <- function(x) {
 }
 
 #' @export
-`addbmm.tensor` <- function(x, batch1, batch2, beta = 1, alpha = 1) {
+addbmm.tensor <- function(x, batch1, batch2, beta = 1, alpha = 1) {
   x$addbmm(batch1, batch2, beta, alpha)
 }
 
 #' @export
-`addcdiv.tensor` <- function(x, tensor1, tensor2, value = 1) {
+addcdiv.tensor <- function(x, tensor1, tensor2, value = 1) {
   x$addcdiv(tensor1, tensor2, value)
 }
 
 #' @export
-`addcmul.tensor` <- function(x, tensor1, tensor2, value = 1) {
+addcmul.tensor <- function(x, tensor1, tensor2, value = 1) {
   x$addcmul(tensor1, tensor2, value)
 }
 
 #' @export
-`addmm.tensor` <- function(x, mat1, mat2, beta = 1, alpha = 1) {
+addmm.tensor <- function(x, mat1, mat2, beta = 1, alpha = 1) {
   x$addmm(mat1, mat2, beta, alpha)
 }
