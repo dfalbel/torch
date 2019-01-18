fdb845ae20774eede27404a0eb2103005c85acd2
Daniel Falbel <dfalbel@gmail.com>
2019-01-18 18:18:00 -0200

    remove crayon dep

--- R/tensor-R6.R
+++ R/tensor-R6.R
@@ -38,7 +38,7 @@
     },
 
     print = function (...){
-      cat(crayon::silver(glue::glue("{cl}", cl = class(self)[[1]])), "\n")
+      cat(glue::glue("{cl}", cl = class(self)[[1]]), "\n")
       tensor_print_(self$pointer)
       invisible(self)
     },
