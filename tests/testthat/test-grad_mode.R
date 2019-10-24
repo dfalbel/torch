54a58e6e37f71f6f2195c7bdcaa4a2c36b578c5b
Daniel Falbel <dfalbel@gmail.com>
2019-10-24 15:55:38 -0300

    remove some tests for now

--- tests/testthat/test-grad_mode.R
+++ tests/testthat/test-grad_mode.R
@@ -7,6 +7,6 @@ test_that("grad_mode works", {
   x <- tensor(runif(100), requires_grad = TRUE)
   s <- tensor(runif(100))
 
-  expect_error(x$sub_(s))
-  expect_silent(with_no_grad(x$sub_(s)))
+  expect_error(x$sub_(s$pointer))
+  expect_silent(with_no_grad(x$sub_(s$pointer)))
 })
